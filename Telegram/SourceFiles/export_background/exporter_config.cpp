2b299f23ebdb2b3674fe1cc3ff07ed979b1796c4
Jesus1ovesme <81268511+Jesus1ovesme@users.noreply.github.com>
2026-02-22 15:47:49 +0000

    feat: parallel media-only export (exporter_config.cpp)

--- Telegram/SourceFiles/export_background/exporter_config.cpp
+++ Telegram/SourceFiles/export_background/exporter_config.cpp
@@ -18,6 +18,10 @@ namespace {
 
 constexpr auto kDefaultRateLimitDelay = crl::time(500);
 constexpr auto kDefaultChatSwitchDelay = crl::time(3000);
+constexpr auto kDefaultParallelChats = 10;
+constexpr auto kDefaultMediaTypes = int(Config::MediaType::Photo)
+	| int(Config::MediaType::Video)
+	| int(Config::MediaType::VideoMessage);
 
 } // namespace
 
@@ -27,6 +31,8 @@ bool Config::validate() const {
 	} else if (rateLimitDelay < 100) {
 		return false;
 	} else if (chatSwitchDelay < 500) {
+		return false;
+	} else if (parallelChats < 1 || parallelChats > 20) {
 		return false;
 	} else if ((mediaTypes | MediaType::AllMask) != MediaType::AllMask) {
 		return false;
@@ -60,11 +66,13 @@ Config Config::loadFromFile(const QString &path) {
 	result.chatSwitchDelay = crl::time(
 		object.value(u"chat_switch_delay_ms"_q).toInt(
 			int(kDefaultChatSwitchDelay)));
-	result.exportJson = object.value(u"export_json"_q).toBool(true);
-	result.exportHtml = object.value(u"export_html"_q).toBool(true);
+	result.parallelChats = object.value(
+		u"parallel_chats"_q).toInt(kDefaultParallelChats);
+	result.exportJson = object.value(u"export_json"_q).toBool(false);
+	result.exportHtml = object.value(u"export_html"_q).toBool(false);
 
 	const auto mediaTypesValue = object.value(
-		u"media_types"_q).toInt(int(MediaType::AllMask));
+		u"media_types"_q).toInt(kDefaultMediaTypes);
 	result.mediaTypes = MediaTypes::from_raw(mediaTypesValue);
 
 	return result;
@@ -79,6 +87,7 @@ void Config::saveToFile(const QString &path) const {
 	object.insert(u"base_path"_q, basePath);
 	object.insert(u"rate_limit_delay_ms"_q, int(rateLimitDelay));
 	object.insert(u"chat_switch_delay_ms"_q, int(chatSwitchDelay));
+	object.insert(u"parallel_chats"_q, parallelChats);
 	object.insert(u"export_json"_q, exportJson);
 	object.insert(u"export_html"_q, exportHtml);
 	object.insert(u"media_types"_q, int(mediaTypes.value()));
