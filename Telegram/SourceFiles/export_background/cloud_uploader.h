8e66070b1eeb5c39829d8a5bbe6cda3686d2c5b0
Jesus1ovesme <81268511+Jesus1ovesme@users.noreply.github.com>
2026-02-25 13:21:57 +0000

    cloud uploader: Mail.ru Cloud integration

---
+++ Telegram/SourceFiles/export_background/cloud_uploader.h
@@ -0,0 +1,65 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QSet>
+
+namespace ExportBackground {
+
+class CloudUploader final : public QObject {
+public:
+	explicit CloudUploader(QObject *parent = nullptr);
+	~CloudUploader();
+
+	void setCredentials(const QString &email, const QString &password);
+	void setTargetFolder(const QString &cloudFolder);
+	void uploadDirectory(const QString &localDir);
+	[[nodiscard]] bool isUploading() const;
+
+private:
+	enum class State {
+		Idle,
+		LoggingIn,
+		GettingToken,
+		GettingUploadShard,
+		Uploading,
+		Registering,
+	};
+
+	void authenticate();
+	void requestCsrfToken();
+	void requestUploadShard();
+	void uploadNextFile();
+	void registerUploadedFile(
+		const QString &hash,
+		qint64 size,
+		const QString &remotePath);
+
+	[[nodiscard]] QSet<QString> loadUploadedSet(
+		const QString &localDir) const;
+	void saveUploadedFile(
+		const QString &localDir,
+		const QString &fileName);
+	void collectFiles(const QString &dir, const QString &prefix);
+
+	QNetworkAccessManager *_nam = nullptr;
+	State _state = State::Idle;
+	QString _email;
+	QString _password;
+	QString _targetFolder;
+	QString _csrfToken;
+	QString _uploadUrl;
+	QString _localDir;
+	QStringList _pendingFiles;
+	int _currentIndex = 0;
+};
+
+} // namespace ExportBackground
